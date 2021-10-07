79e991370c0bb2c0c4c6db551fac1f577097dd90
SuitableEmu <73889862+SuitableEmu@users.noreply.github.com>
2021-10-07 22:17:05 +0200

    Update Cooardinate-getter.ps1

--- Cooardinate-getter.ps1
+++ Cooardinate-getter.ps1
@@ -1,5 +1,5 @@
 #Launch the game hover your cursor over the blue buttons and run the script
-#You will get X and Y *X: 1830 | Y: 878*, do that 3 times and replace them in NewWorldFA.ps1
+#You will get X and Y *X: 1830 | Y: 878*, do that for the 3 buttons and replace them in NewWorldFA.ps1
 
 $cursor = [system.form.cursor]::Clip
 
