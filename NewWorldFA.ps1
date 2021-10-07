3efdaab8a293d93bb430b988799d36a8d4b07971
SuitableEmu <73889862+SuitableEmu@users.noreply.github.com>
2021-10-07 19:50:21 +0200

    Add files via upload
    NewWorls.ps1 only keeps you online in the game
    
    NewWorldFA.ps1 launches and logs you in then keeps you online in the game

---
+++ NewWorldFA.ps1
@@ -0,0 +1,74 @@
+﻿param($minutes = 9999999)
+$Game = "steam://rungameid/1063730"
+$Cursor = [system.windows.forms.cursor]::clip
+#import mouse_event
+Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32 -Namespace W;
+
+
+$wsh = New-Object -ComObject Wscript.Shell
+function Set-WindowStyle {
+param(
+    [Parameter()]
+    [ValidateSet('FORCEMINIMIZE', 'HIDE', 'MAXIMIZE', 'MINIMIZE', 'RESTORE', 
+                 'SHOW', 'SHOWDEFAULT', 'SHOWMAXIMIZED', 'SHOWMINIMIZED', 
+                 'SHOWMINNOACTIVE', 'SHOWNA', 'SHOWNOACTIVATE', 'SHOWNORMAL')]
+    $Style = 'SHOW',
+    [Parameter()]
+    $MainWindowHandle = (Get-Process -Id $pid).MainWindowHandle
+)
+    $WindowStates = @{
+        FORCEMINIMIZE   = 11; HIDE            = 0
+        MAXIMIZE        = 3;  MINIMIZE        = 6
+        RESTORE         = 9;  SHOW            = 5
+        SHOWDEFAULT     = 10; SHOWMAXIMIZED   = 3
+        SHOWMINIMIZED   = 2;  SHOWMINNOACTIVE = 7
+        SHOWNA          = 8;  SHOWNOACTIVATE  = 4
+        SHOWNORMAL      = 1
+    }
+    Write-Verbose ("Set Window Style {1} on handle {0}" -f $MainWindowHandle, $($WindowStates[$style]))
+
+    $Win32ShowWindowAsync = Add-Type –memberDefinition @” 
+    [DllImport("user32.dll")] 
+    public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+“@ -name “Win32ShowWindowAsync” -namespace Win32Functions –passThru
+
+    $Win32ShowWindowAsync::ShowWindowAsync($MainWindowHandle, $WindowStates[$Style]) | Out-Null
+}
+function Start-Sleep($seconds) {
+    $doneDT = (Get-Date).AddSeconds($seconds)
+    while($doneDT -gt (Get-Date)) {
+        $secondsLeft = $doneDT.Subtract((Get-Date)).TotalSeconds
+        $percent = ($seconds - $secondsLeft) / $seconds * 100
+        Write-Progress -Activity "Sleeping" -Status "Sleeping..." -SecondsRemaining $secondsLeft -PercentComplete $percent
+        [System.Threading.Thread]::Sleep(10)
+    }
+   Write-Progress -Activity "Sleeping" -Status "Sleeping..." -SecondsRemaining 0 -Completed
+}
+
+Start-Process $Game 
+
+Start-Sleep 70
+[system.windows.forms.cursor]::position = New-Object System.Drawing.Point(2567,1394)
+Start-Sleep 5
+[W.U32]::mouse_event(6,0,0,0,0);
+Start-Sleep 5
+[system.windows.forms.cursor]::position = New-Object System.Drawing.Point(3000,1400)
+Start-Sleep 2
+[W.U32]::mouse_event(6,0,0,0,0);
+Start-Sleep 5
+[system.windows.forms.cursor]::position = New-Object System.Drawing.Point(3000,1400)
+Start-Sleep 2
+[W.U32]::mouse_event(6,0,0,0,0);
+Start-Sleep 120
+[system.windows.forms.cursor]::position = New-Object System.Drawing.Point(1700,800)
+
+
+for ($i = 0; $i -lt $minutes; $i++) {
+(Get-Process -Name NewWorld).MainWindowHandle | foreach { Set-WindowStyle MAXIMIZE $_ }
+(New-Object -ComObject WScript.Shell).AppActivate((get-process NewWorld).MainWindowTitle)
+#left mouse click
+[W.U32]::mouse_event(6,0,0,0,0);
+Start-Sleep 2
+(Get-Process -Name NewWorld).MainWindowHandle | foreach { Set-WindowStyle MINIMIZE $_ }
+Start-Sleep 900
+}
