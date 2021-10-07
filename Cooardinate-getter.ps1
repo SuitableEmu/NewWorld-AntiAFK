11abd9e35f5da9738e68b668b25995cf447eca32
SuitableEmu <73889862+SuitableEmu@users.noreply.github.com>
2021-10-07 22:11:54 +0200

    Create Cooardinate-getter.ps1

---
+++ Cooardinate-getter.ps1
@@ -0,0 +1,6 @@
+#Launch the game hover your cursor over the blue buttons and run the script
+#You will get X and Y *X: 1830 | Y: 878*, do that 3 times and replace them in NewWorldFA.ps1
+
+$cursor = [system.form.cursor]::Clip
+
+[system.windows.forms.cursor]::position
